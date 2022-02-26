5643e7ca7533492a0fbbba01100e64a138ba4d08
KUSAMPUDI SOUMITH VARMA <61532531+SoumithVarmaK@users.noreply.github.com>
2022-02-27 02:07:47 +0530

    Add files via upload

---
+++ src/components/pages/Trending.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Trending() {
+  return <h1 className='trending'> NO WAY HOME CONTINUES ITS AMAZING BOX-OFFICE RUN </h1>;
+return <h1 className = 'trending'> It's very well done... It's darker and meaner than the original Agatha Christie  </h1>;
+}
---
+++ src/components/pages/Upcoming.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import '../../App.css';
+
+export default function Upcoming() {
+  return <h1 className='upcoming'>BATMAN IS ALMOST HERE</h1>;
+}
